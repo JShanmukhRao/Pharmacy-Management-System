b4aea430db1ec1c9a89b0cf51bafe0f3f17d6cbb
shubham <shubhamrao1233@gmail.com>
2020-04-06 14:54:08 +0530

    first commit

---
+++ includes/about.php
@@ -0,0 +1,88 @@
+<!-- about -->
+	<div class="about">
+		<div class="container">			
+			
+			<!-- team -->
+			<div class="team">
+				<h3>Our Team Members</h3>
+				<div class="team-info">
+					<div class="col-md-3 col-xs-6 team-grids">
+				   <div style="font-weight: bold;font-size: 20px;">		
+					Anil Kumar Yadav <br> <br>
+					Department: CSE <br><br>
+                    </div>
+                    
+						<div class="w3team-gridinfo"> 
+							<img class="img-responsive" src="images/img3.jpg" alt="">
+							<div class="captn">
+								<h4>Welcome</h4>
+								<p></p>
+								<ul class="w3-agileitsicons">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a class="twt" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a class="drbl" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a class="be" href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul> 
+							</div> 
+						</div>					
+					</div>					
+					<div class="col-md-3 col-xs-6 team-grids"> 
+                    <div style="font-weight: bold;font-size: 20px;">		
+					J Shanmukh Rao <br> <br>
+					Department: CSE <br><br>
+                    </div>
+						<div class="w3team-gridinfo"> 
+							
+							<img class="img-responsive" src="images/img4.jpg" alt="">
+							<div class="captn">
+								<h4>Hey</h4>
+								<p></p>
+								
+							</div> 
+						</div> 
+					</div>
+					<div class="col-md-3 col-xs-6 team-grids">
+						<div style="font-weight: bold;font-size: 20px;">		
+					Krishna Reddy <br> <br>
+					Department: CSE <br><br>
+                    </div>
+						<div class="w3team-gridinfo"> 
+							<img class="img-responsive" src="images/img5.jpg" alt="">
+							<div class="captn">
+								<h4></h4>
+								<p></p>
+								<ul class="w3-agileitsicons">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a class="twt" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a class="drbl" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a class="be" href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div> 
+						</div>
+					</div>
+					<div class="col-md-3 col-xs-6 team-grids">
+						<div style="font-weight: bold;font-size: 20px;">		
+					Tejpal Chaudhary <br> <br>
+					Department: CSE <br><br>
+                    </div>
+						<div class="w3team-gridinfo"> 
+							<img class="img-responsive" src="images/img6.jpg" alt="">
+							<div class="captn">
+								<h4></h4>
+								<p></p>
+								<ul class="w3-agileitsicons">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a class="twt" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a class="drbl" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a class="be" href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<!-- //team -->
+		</div>
+	</div>
+	<!-- about -->
---
+++ includes/contact.php
@@ -0,0 +1,20 @@
+<!-- contact -->
+	<div class="contact">
+		<div class="container">				
+			<div class="contact-grids">
+				
+				<div class="col-md-4 address">
+					<h3>Contact info</h3>
+					<p class="cnt-p"> </p>
+					<p>Anil Kumar Yadav</p>
+					<p>Jalandhar</p>
+					<p>Phone : 700713644o</p>
+					
+					<p>Email : <a href="anil:example@mail.com">anil9080yadav@gmail.com</a></p>
+				</div>									
+				<div class="clearfix"> </div>	
+			</div>
+		
+		</div>		
+	</div>
+	<!--//contact-->
